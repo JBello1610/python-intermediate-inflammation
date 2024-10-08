d3e0740db113747dd13ae725192f6cdda6cba59f
tw8671 <john.bello-oro@ukaea.uk>
2024-10-08 14:38:27 +0100

    Add parameterisation mean, min, max test cases

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -14,11 +14,10 @@ def analyse_data(data_dir):
     Gets all the inflammation csvs within a directory, works out the mean
     inflammation value for each day across all datasets, then graphs the
     standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+    data_file_paths = glob.glob(os.path.join(data_dir, "inflammation*.csv"))
     if len(data_file_paths) == 0:
         raise ValueError(f"No inflammation csv's found in path {data_dir}")
     data = map(models.load_csv, data_file_paths)
-
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
@@ -26,6 +25,6 @@ def analyse_data(data_dir):
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
 
     graph_data = {
-        'standard deviation by day': daily_standard_deviation,
+        "standard deviation by day": daily_standard_deviation,
     }
     views.visualize(graph_data)
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,37 +3,54 @@
 import numpy as np
 import numpy.testing as npt
 import os
+import pytest
 
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ],
+)
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
 
 def test_load_from_json(tmpdir):
     from inflammation.models import load_json
-    example_path = os.path.join(tmpdir, 'example.json')
-    with open(example_path, 'w') as temp_json_file:
+
+    example_path = os.path.join(tmpdir, "example.json")
+    with open(example_path, "w") as temp_json_file:
         temp_json_file.write('[{"observations":[1, 2, 3]},{"observations":[4, 5, 6]}]')
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [5, 6]),
+    ],
+)
+def test_daily_max(test, expected):
+    from inflammation.models import daily_max
+
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [1, 2]),
+    ],
+)
+def test_daily_min(test, expected):
+    from inflammation.models import daily_min
+
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
